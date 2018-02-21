f9ace82ee2b4b98b0c6bde140e3b94b705755a31
unjfsc152027 <36284898+unjfsc152027@users.noreply.github.com>
2018-02-21 09:46:11 -0500

    Estructura inicial de C++ con Netbeans 8.2

---
+++ BVAC++EVL0101v01Estructura/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Alex
+ *
+ * Created on 2 de febrero de 2018, 09:15 AM
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 1. Estructura básica de un proyecto C++
+ */
+int main(int argc, char** argv) {
+    printf("BBVA : Arquitectura Host");
+    return 0;
+}
+
