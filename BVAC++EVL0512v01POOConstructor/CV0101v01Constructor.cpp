49a3f70d92fadf7eddea26ef2f09814c5418cd9f
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-04-19 09:51:04 -0500

    POO XV: Constructor para clase Tiempo

--- BVAC++EVL0512v01POOConstructor/CV0101v01Constructor.cpp
+++ BVAC++EVL0512v01POOConstructor/CV0101v01Constructor.cpp
@@ -22,6 +22,7 @@ private:
     int minuto;
     int segundo;
 public:
+    CTTiempo();
     CTTiempo(int=12, int=0, int=0);
     void establecerHora(int, int, int);
     void imprimeUniversal();
@@ -29,6 +30,29 @@ public:
     void capturaHora();
 };
 
+// El constructor CTTiempo inicializa cada dato miembro en cero y garantiza que
+// los objetos CTTiempo comiencen con el estado consistente
+CTTiempo::CTTiempo(int pHora, int pMinuto, int pSegundo){
+    establecerHora(pHora, pMinuto, pSegundo);
+}
+
+void CTTiempo::capturaHora(){
+    int iHora, iMinuto, iSegundo;
+    
+    cout << "Hora : ";
+    cin >> iHora;
+    cout << "Minuto : ";
+    cin >> iMinuto;
+    cout << "Segundo : ";
+    cin >> iSegundo;
+    establecerHora(iHora, iMinuto, iSegundo);
+}
+
+void CTTiempo::establecerHora(int pHora, int pMinuto, int pSegundo){
+    hora = (pHora >= 0 && pHora < 24)?pHora:0;
+    minuto = (pMinuto >= 0 && pHora < 60)?pHora:0;
+    segundo = (pSegundo >= 0 && pHora < 60)?pHora:0;
+}
 int main(int argc, char** argv) {
     cout << "A que hora se levanta Ud.?: " << endl;
     cout << "\nen Hora universal: ";
